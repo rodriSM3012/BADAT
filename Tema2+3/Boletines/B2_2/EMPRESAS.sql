d80988fc158ad622241bd823db90627736a55a53
rodrigosanchez <30.rodrigo.12@gmail.com>
2025-01-22 10:41:40 +0100

    added EMPRESAS.sql

---
+++ Tema2+3/Boletines/B2_2/EMPRESAS.sql
@@ -0,0 +1,24 @@
+drop schema EMPRESAS;
+
+create schema EMPRESAS;
+
+use EMPRESAS;
+
+create table EMPLEADOS (
+    CEMPL int,
+    NOMBRE varchar(15),
+    CARGO varchar(15),
+    JEFE int,
+    FALTA date,
+    SALARIO decimal(7, 2),
+    COMISION decimal(7, 2),
+    DEPT int,
+    primary key (CEMPL)
+);
+
+create table DEPARTAMENTOS (
+    DEPT int,
+    NOMBRE varchar(15),
+    CIUDAD varchar(15),
+    primary key (DEPT)
+);
