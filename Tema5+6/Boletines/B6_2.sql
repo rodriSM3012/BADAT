f50d7440738af81b7115a57e83eb217d533e6909
rodrism3012 <30.rodrigo.12@gmail.com>
2025-05-13 16:18:25 +0200

    Add PL/SQL blocks to validate article pricing and find the highest priced article

--- Tema5+6/Boletines/B6_2.sql
+++ Tema5+6/Boletines/B6_2.sql
@@ -28,3 +28,60 @@ BEGIN
     END LOOP;
     COMMIT;
 END;
+
+-- 3.
+CREATE TABLE TABLA_ARTICULOS (
+    CODIGO VARCHAR2(20),
+    NOMBRE VARCHAR2(30),
+    PRECIO NUMBER(5, 2),
+    IVA NUMBER(5, 2)
+)
+
+INSERT INTO TABLA_ARTICULOS VALUES ('A001', 'LAPIZ', 13, 8);
+INSERT INTO TABLA_ARTICULOS VALUES ('A002', 'BOLIGRAFO', 20, 8);
+INSERT INTO TABLA_ARTICULOS VALUES ('A003', 'LIBRETA', 5, 8);
+INSERT INTO TABLA_ARTICULOS VALUES ('A004', 'PLUMA', 63, 17);
+INSERT INTO TABLA_ARTICULOS VALUES ('A005', 'SACAPUNTAS', 5, 17);
+
+-- A). Construye un bloque PL/SQL que compruebe si el precio con IVA del artículo
+--     cuyo código es 'A001' es mayor de 10 euros y en caso afirmativo imprima el
+--     nombre y el precio del artículo por pantalla. Si es menor de 10€ imprimirá un
+--     mensaje indicándolo. Si no existe el artículo imprimirá también un mensaje
+--     indicándolo.
+
+DECLARE 
+    vNom TABLA_ARTICULOS.NOMBRE % TYPE;
+    vPrecio TABLA_ARTICULOS.PRECIO % TYPE;
+    vPVP NUMBER(8, 2);
+BEGIN
+    SELECT PRECIO + IVA, NOMBRE, PRECIO 
+    INTO vPVP, vNom, vPrecio
+    FROM TABLA_ARTICULOS
+    WHERE CODIGO = 'A001';
+    
+    IF (vPVP >= 10) THEN 
+        DBMS_OUTPUT.put_line('El articulo ' || vNom || ' tiene un precio de ' || vPrecio); 
+    ELSIF (vPVP < 10) THEN 
+        DBMS_OUTPUT.put_line('Precio es menor que 10');
+    ELSE 
+        DBMS_OUTPUT.put_line('No existe');
+    END IF;
+END;
+
+-- B). Construye un bloque que seleccione el artículo de mayor precio que estés
+--    almacenado en la tabla, guarde su nombre en una variable y luego lo muestre.
+
+DECLARE 
+    vNom TABLA_ARTICULOS.NOMBRE % TYPE;
+    vAux TABLA_ARTICULOS.PRECIO % TYPE := 0;
+BEGIN
+    FOR i IN (SELECT PRECIO, NOMBRE FROM TABLA_ARTICULOS) LOOP 
+        IF PRECIO > vAux THEN
+            vAux := PRECIO;
+            vNom := NOMBRE;
+        END IF;
+    END LOOP; 
+    DBMS_OUTPUT.put_line('El objeto con nombre "' || vNom || '" tiene el mayor precio con valor de ' || vAux || '.');
+END;
+
+SELECT MAX(PRECIO)
